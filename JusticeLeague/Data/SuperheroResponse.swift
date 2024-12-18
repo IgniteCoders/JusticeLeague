9fd6587cc7bdee0cff8405eccbe285d968739263
Mansour <theboom.kajas@gmail.com>
2024-12-18 18:55:07 +0100

    We used CodingKeys to show an example with the Biography

--- JusticeLeague/Data/SuperheroResponse.swift
+++ JusticeLeague/Data/SuperheroResponse.swift
@@ -15,6 +15,20 @@ struct Superhero: Codable {
     let id: String
     let name: String
     let image: Image
+    let biography: Biography
+}
+
+struct Biography: Codable {
+    let realName: String
+    let publisher: String
+    let alignment: String
+    let placeOfBirth: String
+    
+    private enum CodingKeys : String, CodingKey {
+        case alignment, publisher
+        case realName = "full-name"
+        case placeOfBirth = "place-of-birth"
+    }
 }
 
 struct Image: Codable {
